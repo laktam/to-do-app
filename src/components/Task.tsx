3808ebd6643629a63247af696ea279f8ca97b409
laktam abdellah <laktamlaktamabdellah@gmail.com>
2023-08-17 20:07:25 +0100

    update Task

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,17 +1,27 @@
-import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
-  border-radius: 10px;
+  width: 200px;
+  background-color: grey;
+  border-radius: 6px;
   padding: 10px;
 `;
 const SubContainer = styled.div`
+  margin-top: 8px;
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
 `;
 const Content = styled.div``;
-const Time = styled.div``;
-const Done = styled.button``;
+const Time = styled.div`
+  background-color: white;
+  border-radius: 20px;
+  padding: 5px 10px;
+`;
+const Done = styled.button`
+  border-radius: 20px;
+  padding: 5px 10px;
+  font-size: 16px;
+`;
 
 type Props = {
   content: string;
